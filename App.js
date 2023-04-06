a55f024f327b2752729233f78416ac6a1eeb3de9
nerdnightboy <nerdnightboy@.com>
2023-04-06 15:53:22 +0530

    Having Error "Invalid attemp to destructure non-iterable instance"

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Camera } from 'expo-camera';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+
+  const [hascampermission, sethascampermission] = useState(null);
+  const [camera, setCamera] = useState[null];
+  const [image,setImage] = useState[null];
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(()=>{
+    (async()=>{
+      const camStatus = await Camera.requestCameraPermissionsAsync();
+      sethascampermission(camStatus.status === "granted");
+    })();
+  },[])
+
+  const takePic = async () =>{
+    if(camera){
+      const data = await camera.takePicAsync();
+      setImage(data.uri);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.cameracontainer}>
+        <Camera style={styles.camera} type={type} ratio={'1:1'} ref={(ref) => setCamera(ref)} />
+      </View>
+      <View style={{flex:0, flexDirection:'row', justifyContent:'center',}}>
+        <TouchableOpacity onPress={()=>{
+          setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back)
+        }}>
+          <Image style={styles.icon} source={require('./images/pngegg.png')}/>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => takePic()}>
+          <Image style={styles.icon} source={require('./images/pngwing.com.png')}/>
+        </TouchableOpacity>
+      </View>
+      {image && <Image source={{uri:image}}  style={{flex:1,}}/>}
     </View>
   );
 }
@@ -13,8 +47,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
   },
+  icon:{
+    width:50,
+    height:50,
+    margin:10,
+  },
+  cameracontainer:{
+    flex:1,
+
+  },
+  camera:{
+    flex:1, 
+    aspectRatio:1,
+  }
 });
